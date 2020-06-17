04b370bc32920fe0d78eee5c424b3201c64cd995
zhousb1999 <2080211280@qq.com>
2020-06-17 18:02:14 +0800

    暂时提交

--- src/main/java/priv/zhou/domain/dto/BlogDTO.java
+++ src/main/java/priv/zhou/domain/dto/BlogDTO.java
@@ -1,5 +1,6 @@
 package priv.zhou.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -61,11 +62,13 @@ public class BlogDTO extends DTO<BlogPO> {
     /**
      * 创建时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date gmtCreate;
 
     /**
      * 修改时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date gmtModified;
 
 
