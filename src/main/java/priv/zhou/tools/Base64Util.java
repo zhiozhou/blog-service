5efd95307ab24b771799b700b85f834abbef2207
zhou <2080211280@qq.com>
2020-07-10 17:18:54 +0800

    修改base64工具

--- src/main/java/priv/zhou/tools/Base64Util.java
+++ src/main/java/priv/zhou/tools/Base64Util.java
@@ -1,17 +1,15 @@
 package priv.zhou.tools;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
+import java.util.Base64;
 
 
 public class Base64Util {
-
 
     /**
      * BASE64解密
      */
     public static byte[] decode(String key) throws Exception {
-        return null == key ? null : new BASE64Decoder().decodeBuffer(key);
+        return null == key ? null : Base64.getDecoder().decode(key);
     }
 
 
@@ -19,6 +17,6 @@ public class Base64Util {
      * BASE64加密
      */
     public static String encode(byte[] key) {
-        return null == key ? null : new BASE64Encoder().encodeBuffer(key);
+        return null == key ? null : Base64.getEncoder().encodeToString(key);
     }
 }
